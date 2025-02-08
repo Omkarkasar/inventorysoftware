9da55e3cb159f3295f66bc54a093f96da14e3ccf
Omkar Kasar <omkarkasar101010@gmail.com>
2025-02-08 11:26:06 +0530

    client

--- resources/views/productform.blade.php
+++ resources/views/productform.blade.php
@@ -23,6 +23,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="exampleModal1Label">Add New User</h1>
+                    <h1 >Add New User</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
